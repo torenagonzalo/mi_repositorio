dd6e8333211d985b132c40a666a49b3385d401ef
torenagonzalo <102437220+torenagonzalo@users.noreply.github.com>
2022-07-18 17:26:48 -0300

    Add files via upload

---
+++ shared/mockdata.js
@@ -0,0 +1,241 @@
+export const mockData = [{
+    "id": 1,
+    "title": "German Cockroach",
+    "price": "$9.78",
+    "category": "china-unionpay",
+    "description": "Postinflam optic atrophy"
+}, {
+    "id": 2,
+    "title": "DOXERCALCIFEROL",
+    "price": "$2.16",
+    "category": "jcb",
+    "description": "Arterial embolism NEC"
+}, {
+    "id": 3,
+    "title": "fluorometholone acetate",
+    "price": "$9.27",
+    "category": "jcb",
+    "description": "Accident d/t dust storm"
+}, {
+    "id": 4,
+    "title": "Oxymetazoline HCl",
+    "price": "$5.58",
+    "category": "maestro",
+    "description": "Aqueous misdirection"
+}, {
+    "id": 5,
+    "title": "eucalyptol, menthol, methyl salicylate, thymol",
+    "price": "$6.76",
+    "category": "jcb",
+    "description": "Opn skul/oth fx-coma NOS"
+}, {
+    "id": 6,
+    "title": "Famotidine",
+    "price": "$9.59",
+    "category": "mastercard",
+    "description": "Adv eff antidepressants"
+}, {
+    "id": 7,
+    "title": "Diltiazem Hydrochloride",
+    "price": "$1.70",
+    "category": "jcb",
+    "description": "Malignant otitis externa"
+}, {
+    "id": 8,
+    "title": "Ignatia Bryophyllum",
+    "price": "$2.76",
+    "category": "maestro",
+    "description": "Allerg arthritis-shlder"
+}, {
+    "id": 9,
+    "title": "SODIUM FLUORIDE F-18",
+    "price": "$7.58",
+    "category": "americanexpress",
+    "description": "H simplex otitis externa"
+}, {
+    "id": 10,
+    "title": "CALENDULA OFFICINALIS FLOWER, SAMBUCUS NIGRA FLOWER, ECHINACEA ANGUSTIFOLIA, THUJA OCCIDENTALIS",
+    "price": "$7.16",
+    "category": "maestro",
+    "description": "Pelvic congestion synd"
+}, {
+    "id": 11,
+    "title": "PYRITHIONE ZINC",
+    "price": "$2.26",
+    "category": "jcb",
+    "description": "Trans-sexualism NOS"
+}, {
+    "id": 12,
+    "title": "Norepinephrine bitartrate",
+    "price": "$9.51",
+    "category": "switch",
+    "description": "Alcoholic cardiomyopathy"
+}, {
+    "id": 13,
+    "title": "Hydrocodone Bitartrate and Acetaminophen",
+    "price": "$7.49",
+    "category": "jcb",
+    "description": "Chr margin ulc perf-obst"
+}, {
+    "id": 14,
+    "title": "ramucirumab",
+    "price": "$1.79",
+    "category": "jcb",
+    "description": "Mal neo lacrimal gland"
+}, {
+    "id": 15,
+    "title": "HYDROGENATED SOYBEAN",
+    "price": "$7.34",
+    "category": "laser",
+    "description": "Oth ob surg compl-postpa"
+}, {
+    "id": 16,
+    "title": "Pantoprazole Sodium",
+    "price": "$4.52",
+    "category": "visa-electron",
+    "description": "Ben crcinoid foregut NOS"
+}, {
+    "id": 17,
+    "title": "topiramate",
+    "price": "$6.50",
+    "category": "jcb",
+    "description": "Benign hypertension"
+}, {
+    "id": 18,
+    "title": "Benzocaine",
+    "price": "$6.71",
+    "category": "visa",
+    "description": "Malignant pleural effusn"
+}, {
+    "id": 19,
+    "title": "Diphenhydramine Hydrochloride, Zinc Acetate",
+    "price": "$9.34",
+    "category": "visa",
+    "description": "Abdmnal mass rt lwr quad"
+}, {
+    "id": 20,
+    "title": "Fluconazole",
+    "price": "$1.53",
+    "category": "jcb",
+    "description": "Opn skull vlt fx/hem NEC"
+}, {
+    "id": 21,
+    "title": "Cilostazol",
+    "price": "$7.06",
+    "category": "jcb",
+    "description": "Undetermin circ-hanging"
+}, {
+    "id": 22,
+    "title": "Lamotrigine",
+    "price": "$3.08",
+    "category": "americanexpress",
+    "description": "Bubonic plague"
+}, {
+    "id": 23,
+    "title": "ARIPIPRAZOLE",
+    "price": "$4.44",
+    "category": "jcb",
+    "description": "Radiation exposure NEC"
+}, {
+    "id": 24,
+    "title": "Aconite, Antimonium Tart , Arnica , Carbo Veg",
+    "price": "$9.92",
+    "category": "visa",
+    "description": "Retinoph prematr,stage 5"
+}, {
+    "id": 25,
+    "title": "Hackberry Celtis occidentalis",
+    "price": "$9.18",
+    "category": "jcb",
+    "description": "Lt-for-dates 1750-1999g"
+}, {
+    "id": 26,
+    "title": "Alder, Red Alnus rubra",
+    "price": "$6.85",
+    "category": "china-unionpay",
+    "description": "Unilat cleft palate-comp"
+}, {
+    "id": 27,
+    "title": "Anastrozole",
+    "price": "$4.65",
+    "category": "jcb",
+    "description": "3rd brn w loss-upper arm"
+}, {
+    "id": 28,
+    "title": "Hydrocodone Bitartrate and Ibuprofen",
+    "price": "$2.67",
+    "category": "jcb",
+    "description": "Coccidiosis"
+}, {
+    "id": 29,
+    "title": "telmisartan/amlodipine",
+    "price": "$6.78",
+    "category": "bankcard",
+    "description": "Toxic effect nitroglycol"
+}, {
+    "id": 30,
+    "title": "Ziprasidone Hydrochloride",
+    "price": "$2.68",
+    "category": "jcb",
+    "description": "Mallet finger"
+}, {
+    "id": 31,
+    "title": "Aconitum napellus, Cantharis, Gelsemium sempervirens, Natrum sulphuricum, Picricum acidum, Pulsatilla, Sepia, Staphysagria",
+    "price": "$1.97",
+    "category": "jcb",
+    "description": "Antepart hem NEC-unspec"
+}, {
+    "id": 32,
+    "title": "ALUMINUM ZIRCONIUM TETRACHLOROHYDREX GLY",
+    "price": "$1.39",
+    "category": "visa-electron",
+    "description": "Cl skl vlt fx-proln coma"
+}, {
+    "id": 33,
+    "title": "zolpidem tartrate",
+    "price": "$8.54",
+    "category": "visa-electron",
+    "description": "W/craft fall NOS-skier"
+}, {
+    "id": 34,
+    "title": "albuterol sulfate",
+    "price": "$6.01",
+    "category": "laser",
+    "description": "Open wound of pharynx"
+}, {
+    "id": 35,
+    "title": "Echinacea, Phytolacca Decandra, Kali Iodatum, Pinus Sylvestris, Pituitary, Apis Mellifica, Aranea Diadema",
+    "price": "$1.29",
+    "category": "jcb",
+    "description": "Acc pois-carbn monox NOS"
+}, {
+    "id": 36,
+    "title": "Meteoric Iron Prunus",
+    "price": "$3.90",
+    "category": "mastercard",
+    "description": "Acq ankle-foot def NOS"
+}, {
+    "id": 37,
+    "title": "Candesartan cilexetil",
+    "price": "$1.90",
+    "category": "jcb",
+    "description": "Open wound arm mult/NOS"
+}, {
+    "id": 38,
+    "title": "Oxycodone and Acetaminophen",
+    "price": "$9.53",
+    "category": "maestro",
+    "description": "Cong macular change-eye"
+}, {
+    "id": 39,
+    "title": "ZINC OXIDE",
+    "price": "$7.34",
+    "category": "jcb",
+    "description": "Heredit hemochromatosis"
+}, {
+    "id": 40,
+    "title": "Sennosides",
+    "price": "$7.08",
+    "category": "switch",
+    "description": "Oth spec dis viruses"
+}]
